34f2f820aa9dba27fa7c6e4698d96b053feee278
Timwi <github@timwi.de>
2016-11-02 02:01:23 +0100

    Initial empty.

---
+++ Assets/MysteryMeatModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MysteryMeat;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Mystery Meat
+/// Created by Timwi
+/// </summary>
+public class MysteryMeatModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[MysteryMeat] Started");
+        Module.OnActivate += ActivateModule;
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[MysteryMeat] Activated");
+    }
+}
