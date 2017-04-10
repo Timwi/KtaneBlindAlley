b7e61ba1ae0e3999fd795ce49e8f8980dc34e874
Timwi <github@timwi.de>
2017-04-10 03:34:19 +0200

    Add Twitch Plays support.

--- Assets/BlindAlleyModule.cs
+++ Assets/BlindAlleyModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using BlindAlley;
 using UnityEngine;
 using Rnd = UnityEngine.Random;
@@ -120,5 +121,31 @@ public class BlindAlleyModule : MonoBehaviour
 
         Debug.LogFormat("[Blind Alley #{8}] Region condition counts:\n{0} {1}\n{2} {3} {4}\n{5} {6} {7}", counts.Select(c => c.ToString()).Concat(new[] { _moduleId.ToString() }).ToArray());
         Debug.LogFormat("[Blind Alley #{1}] Must press regions: {0}", string.Join(", ", Enumerable.Range(0, 8).Where(ix => states[ix] == RegionState.Unclicked).Select(ix => regionNames[ix]).ToArray()), _moduleId);
+    }
+
+    KMSelectable[] ProcessTwitchCommand(string command)
+    {
+        var split = command.Trim().ToLowerInvariant().Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+        if (split.Length < 2 || split[0] != "press")
+            return null;
+
+        var btns = new List<KMSelectable>();
+        foreach (var cmd in split.Skip(1))
+            switch (cmd.Replace("center", "middle").Replace("centre", "middle"))
+            {
+                case "tl": case "lt": case "topleft": case "lefttop": btns.Add(Regions[0]); break;
+                case "tm": case "tc": case "mt": case "ct": case "topmiddle": case "middletop": btns.Add(Regions[1]); break;
+
+                case "ml": case "cl": case "lm": case "lc": case "middleleft": case "leftmiddle": btns.Add(Regions[2]); break;
+                case "mm": case "cm": case "mc": case "cc": case "middle": case "middlemiddle": btns.Add(Regions[3]); break;
+                case "mr": case "cr": case "rm": case "rc": case "middleright": case "rightmiddle": btns.Add(Regions[4]); break;
+
+                case "bl": case "lb": case "bottomleft": case "leftbottom": btns.Add(Regions[5]); break;
+                case "bm": case "bc": case "mb": case "cb": case "bottommiddle": case "middlebottom": btns.Add(Regions[6]); break;
+                case "br": case "rb": case "bottomright": case "rightbottom": btns.Add(Regions[7]); break;
+
+                default: return null;
+            }
+        return btns.ToArray();
     }
 }
